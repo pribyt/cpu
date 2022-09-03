9a06405e2a8e1524782321e1d606fdd8560c4233
pribyt.d <pribyt.d@gmail.com>
2022-09-03 21:38:53 +0300

    fix bug

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 FROM debian:latest
 RUN apt-get update
-RUN apt-get upgrade
 RUN apt-get install -y git bc jq nginx-common nginx-extras
 WORKDIR /app/
 RUN git clone https://github.com/pribyt/cpu.git -b main
-CP /app/bash /etc/nginx/sites-enabled/bash 
+COPY ./bash /etc/nginx/sites-enabled/bash 
+COPY . /app/
 RUN chmod +x /app/app.sh
-EXPOSE 1051
+EXPOSE 1501
 STOPSIGNAL SIGQUIT
 CMD ["nginx", "-g", "daemon off;"]
--- app.sh
+++ app.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 
-FREE_DATA=`free -m | grep Mem`
-CURRENT=`echo $FREE_DATA | cut -f3 -d' '`
-TOTAL=`echo $FREE_DATA | cut -f2 -d' '`
-CPU=`top -b -n1 | grep "Cpu(s)" | awk '{print $2 + $4}'`
-RAM=$(echo "scale = 2; $CURRENT/$TOTAL*100" | bc)
+CPU=`grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}'`
+RAM=$(awk '{ printf "%.2f", $2/1024/1024 ; exit}' /proc/meminfo)
 HDD=`df -lh | awk '{if ($6 == "/") { print $5 }}' | head -1 | cut -d'%' -f1`
 
 jq . << EOF
 {
-    "CPU": "$CPU%",
-    "RAM": "$RAM%",
-    "HDD": "$HDD%",
+    "CPU usage": "$CPU",
+    "RAM all": "$RAM Gb",
+    "HDD usage": "$HDD%",
     "kernel_version": "$(uname -r)",
     "operating_system": "$(awk '$1 == "NAME"{printf "%s ", $3 } $1 == "VERSION" {print $3}' FS=[=\"] /etc/os-release)",
     "os_architecture": "$(arch)"
